fb1b319b5527e9cfab8bb01da73519f31a0f819a
Catalin Mihit <catalin.mihit2002@gmail.com>
2025-12-14 14:51:16 +0100

    Fixing library imports?

--- signac/templates/script.sh
+++ signac/templates/script.sh
@@ -7,8 +7,9 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 
+module load LAMMPS/23Jun2022-foss-2021b-kokkos
+module unload SciPy-bundle/2021.10-foss-2021b
 module load Anaconda3/2023.03-1
 conda activate myenv
-module load LAMMPS/23Jun2022-foss-2021b-kokkos
 
 {% endblock %}
