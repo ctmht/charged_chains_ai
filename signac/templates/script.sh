ca5fecd58fa11fe5db7d4e6dfe2cdcd28c341c0b
Catalin Mihit <catalin.mihit2002@gmail.com>
2025-12-25 23:50:52 +0100

    Bash adjustments to avoid HPC module errors

--- signac/templates/script.sh
+++ signac/templates/script.sh
@@ -2,14 +2,16 @@
 {% block header %}
 #!/bin/bash
 #SBATCH --job-name="{{ id }}"
-#SBATCH --output=slurm_outs/slurm-job-%j.out
-#SBATCH --time=00:100:00
+#SBATCH --output=slurm_outs/slurm-run_simulation_job-%j.out
+#SBATCH --time=00:125:00
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 
-module load LAMMPS/23Jun2022-foss-2021b-kokkos
+source /etc/profile.d/modules.sh
+
+module --ignore-cache load LAMMPS/23Jun2022-foss-2021b-kokkos
 module unload SciPy-bundle/2021.10-foss-2021b
-module load Anaconda3/2023.03-1
+module --ignore-cache load Anaconda3/2023.03-1
 conda activate myenv
 
 {% endblock %}
